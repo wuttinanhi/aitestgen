b403e124d8562c78049463c744d338de1c7794cb
wuttinanhi <42175957+wuttinanhi@users.noreply.github.com>
2024-10-03 21:31:56 +0700

    working one!

--- src/generated.ts
+++ src/generated.ts
@@ -3,14 +3,19 @@ import PuppeteerWebTest from "./engines/puppeteer";
 async function generated() {
   const browser = new PuppeteerWebTest();
 
-  await browser.launchBrowser()
-await browser.navigateTo('https://todomvc.com/')
-await browser.clickElement('a[href*="react/dist/"]')
-await browser.expectElementVisible('input.new-todo', true)
-await browser.typeText('input.new-todo', 'Prepare for interview')
-await browser.pressKey('Enter')
-await browser.expectElementText('label[data-testid="todo-item-label"]', 'Prepare for interview')
-await browser.closeBrowser()
+  await browser.launchBrowser();
+  await browser.navigateTo("https://todomvc.com/");
+  await browser.clickElement("a[href*=react]");
+  await browser.typeText("#todo-input", "Prepare for interview");
+  await browser.pressKey("Enter");
+  const b = await browser.expectElementText(
+    "ul.todo-list li:last-child",
+    "Prepare for interview"
+  );
+  console.log(b);
+
+  await browser.closeBrowser();
+  await browser.closeBrowser();
 
   await browser.closeBrowser();
 }
